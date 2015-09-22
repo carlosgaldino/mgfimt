f433395d3690be4f2c810e26a50ee3c0aa00dc43
Carlos Galdino <carloshsgaldino@gmail.com>
2015-09-22 02:08:56 -0300

    Accept input from STDIN.
    Now the command can receive the value input from a pipe.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 extern crate serde_json;
 
 use std::io::prelude::*;
+use std::io;
 use std::env;
 use std::fs::OpenOptions;
 use std::path::Path;
 use serde_json::{Value, ser};
+use std::process;
 
 fn main() {
     let filepath    = Path::new(&env::home_dir().unwrap()).join(".gifs");
@@ -16,8 +18,21 @@ fn main() {
     let mut data: Value = serde_json::from_str(&content).unwrap();
     let mut obj         = data.as_object_mut().unwrap();
 
-    let key   = env::args().skip(1).next().unwrap();
-    let value = env::args().skip(2).next().unwrap();
+    let count = env::args().count();
+    if count < 2 {
+        println!("Usage: {} key value", env::args().next().unwrap());
+        process::exit(1);
+    }
+
+    let key = env::args().skip(1).next().unwrap();
+
+    let mut value = String::new();
+    if count == 3 {
+        value = env::args().skip(2).next().unwrap();
+    } else if count == 2 {
+        io::stdin().read_to_string(&mut value);
+        value = value.trim_right().to_string();
+    };
 
     obj.insert(key, Value::String(value));
 
